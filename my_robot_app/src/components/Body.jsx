1fb0ba180fc0b70326fe4e3dcce7820930df4af5
Mohamed Ahmed <mohammedhassan9748@gmail.com>
2024-03-12 02:49:35 +0200

    Robot Web App

---
+++ my_robot_app/src/components/Body.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Body extends Component{
+
+    render(){
+        return <h1>Welcome, Hello World</h1>
+    }
+}
+
+export default Body;
---
+++ my_robot_app/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Footer extends Component{
+
+    render(){
+        return <p>Footer Menu</p>
+    }
+}
+
+export default Footer;
