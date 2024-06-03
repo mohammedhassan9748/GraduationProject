b0aa25143471cbd386ed74ca38076e511733d5c0
Mohamed Ahmed <mohammedhassan9748@gmail.com>
2024-06-03 18:49:34 +0300

    updating pre deployment

--- my_robot_app/src/scripts/config.js
+++ my_robot_app/src/scripts/config.js
@@ -1,6 +1,6 @@
 const Config = {
-    PROTOCOL: 'ws://',
-    ROSBRIDGE_SERVER_IP: "192.168.100.216",
+    PROTOCOL: 'wss://',
+    ROSBRIDGE_SERVER_IP: "156.214.222.92",
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIMER: 3000,
     CMD_VEL_TOPIC: "/cmd_vel",
